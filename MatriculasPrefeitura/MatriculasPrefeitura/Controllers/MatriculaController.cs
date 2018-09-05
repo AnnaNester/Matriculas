9a745b5ce85bf29cff832725cd5229d4b93f3f29
anna <anna.nester@gmail.com>
2018-09-05 13:36:17 -0300

    Criado Controller e DAO Matricula

---
+++ MatriculasPrefeitura/MatriculasPrefeitura/Controllers/MatriculaController.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MatriculasPrefeitura.Controllers
+{
+    [Authorize]
+    public class MatriculaController : Controller
+    {
+        // GET: Matricula
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public void MatricularAluno()
+        {
+
+        }
+
+        public void ExcluirMatricula()
+        {
+
+        }
+    }
+}
---
+++ MatriculasPrefeitura/MatriculasPrefeitura/DAL/MatriculaDAO.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace MatriculasPrefeitura.DAL
+{
+    public class MatriculaDAO
+    {
+        public void MatricularAluno()
+        {
+
+        }
+
+        public void ExcluirMatricula()
+        {
+
+        }
+    }
+}
