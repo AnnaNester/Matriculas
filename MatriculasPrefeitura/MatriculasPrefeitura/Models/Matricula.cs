9368df3c805d3db96829456183d95eacc8d11132
anna <anna.nester@gmail.com>
2018-09-05 13:20:49 -0300

    Alteração Models e Adicionado Model Matricula

---
+++ MatriculasPrefeitura/MatriculasPrefeitura/Models/Matricula.cs
@@ -0,0 +1,21 @@
+﻿using MatriculasOsorio.Models;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Web;
+
+namespace MatriculasPrefeitura.Models
+{
+    [Table("Matriculas")]
+    public class Matricula
+    {
+        [Key]
+        public int MatriculaId { get; set; }
+
+        public Aluno Aluno { get; set; }
+
+        public Curso Curso { get; set; }
+    }
+}
