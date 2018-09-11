711c1f198da80cf5580291502313e6a7062555e2
anna <anna.nester@gmail.com>
2018-09-11 17:28:42 -0300

    Implementado métodos na CategoriaDAO

--- MatriculasPrefeitura/MatriculasPrefeitura/DAL/CategoriaDAO.cs
+++ MatriculasPrefeitura/MatriculasPrefeitura/DAL/CategoriaDAO.cs
@@ -2,6 +2,7 @@
 using MatriculasPrefeitura.Models;
 using System;
 using System.Collections.Generic;
+using System.Data.Entity;
 using System.Linq;
 using System.Web;
 
@@ -15,19 +16,42 @@ namespace MatriculasPrefeitura.DAL
         {
             return context.Categorias.ToList();
         }
-        public static void CadastrarCategoria()
+        public static bool CadastrarCategoria(CategoriaCurso categoria)
         {
-
+            if(BuscarCategoriaPorNome(categoria) == null)
+            {
+                context.Categorias.Add(categoria);
+                context.SaveChanges();
+                return true;
+            }
+            return false;
         }
 
-        public static void AlterarCategoria()
+        public static CategoriaCurso BuscarCategoriaPorNome(CategoriaCurso categoria)
         {
-
+            return context.Categorias.FirstOrDefault(x => x.NomeCategoria.Equals(categoria.NomeCategoria));
         }
 
-        public static void ExcluirCategoria()
+        public bool  AlterarCategoria(CategoriaCurso categoria)
         {
+            if (context.Categorias.FirstOrDefault(x => x.NomeCategoria.Equals(categoria.NomeCategoria) && x.CategoriaId != categoria.CategoriaId) == null)
+            {
+                context.Entry(categoria).State = EntityState.Modified;
+                context.SaveChanges();
+                return true;
+            }
+            return false;
+        }
 
+        public static void ExcluirCategoria(int id)
+        {
+            context.Categorias.Remove(BuscarCategoriaPorId(id));
+            context.SaveChanges();
+        }
+
+        public static CategoriaCurso BuscarCategoriaPorId(int? id)
+        {
+            return context.Categorias.Find(id);
         }
     }
 }
